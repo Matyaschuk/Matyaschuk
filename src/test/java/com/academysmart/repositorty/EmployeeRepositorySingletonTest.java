b50a267c6fdb0e7b4e5ff2950782030788846617
marina64 <marina13kud@gmail.com>
2014-10-27 14:51:39 +0200

    Create repository

---
+++ src/test/java/com/academysmart/repositorty/EmployeeRepositorySingletonTest.java
@@ -0,0 +1,32 @@
+package com.academysmart.repositorty;
+
+import static org.junit.Assert.assertTrue;
+import javax.servlet.ServletException;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.academysmart.exception.IncorrectEmailException;
+import com.academysmart.repository.EmployeeRepositorySingleton;
+
+public class EmployeeRepositorySingletonTest {
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		EmployeeRepositorySingleton.getRepository().addEmployee("Иван",
+				"Иванов", "ivanov@mail.ru");
+	}
+
+	@Test
+	public void testGetRepositoryReturnOneInstance() {
+		Assert.assertSame(EmployeeRepositorySingleton.getRepository(),
+				EmployeeRepositorySingleton.getRepository());
+	}
+
+	@Test(expected = IncorrectEmailException.class)
+	public void testAddEmployeWithIncorrectEmail() throws ServletException,
+			IncorrectEmailException {
+		EmployeeRepositorySingleton.getRepository().addEmployee("Иван",
+				"Ианов", "ivanovICH@mail.ru");
+		EmployeeRepositorySingleton.getRepository().addEmployee("Иван",
+				"Иванов", "ivanovICH@mail.ru");
+	}
+}
